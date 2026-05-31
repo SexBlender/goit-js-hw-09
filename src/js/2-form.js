a43db949628f9838eb08964b28c9a166f2ef56d8
AntonD <adikhtenko@gmail.com>
2026-05-31 16:34:02 +0300

    hw-9

---
+++ src/js/2-form.js
@@ -0,0 +1,40 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', e => {
+  const { name, value } = e.target;
+
+  formData[name] = value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData.email.trim() === '' || formData.message.trim() === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+
+  formData.email = '';
+  formData.message = '';
+});
+
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+if (savedData) {
+  formData.email = savedData.email || '';
+  formData.message = savedData.message || '';
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
